c745017b5ccd2eab23b5863f9aab2f2316252caa
funshow <136590076@qq.com>
2023-01-07 11:49:33 +0800

    update

---
+++ gateways/web/src/FunShow.WebGateway/FunShowWebGatewayModule.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Rewrite;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using FunShow.Shared.Hosting.AspNetCore;
+using FunShow.Shared.Hosting.Gateways;
+using Volo.Abp;
+using Volo.Abp.Modularity;
+
+namespace FunShow.WebGateway;
+
+[DependsOn(
+    typeof(FunShowSharedHostingGatewaysModule)
+)]
+public class FunShowWebGatewayModule : AbpModule
+{
+    public override void ConfigureServices(ServiceConfigurationContext context)
+    {
+        // Enable if you need hosting environment
+        // var hostingEnvironment = context.Services.GetHostingEnvironment();
+        var configuration = context.Services.GetConfiguration();
+        var hostingEnvironment = context.Services.GetHostingEnvironment();
+
+        SwaggerConfigurationHelper.ConfigureWithAuth(
+            context: context,
+            authority: configuration["AuthServer:Authority"],
+            scopes: new
+                Dictionary<string, string> /* Requested scopes for authorization code request and descriptions for swagger UI only */ {
+                    { "AccountService", "Account Service API" },
+                    { "IdentityService", "Identity Service API" },
+                    { "AdministrationService", "Administration Service API" },
+                    { "LoggingService", "Logging Service API" }
+                },
+            apiTitle: "Web Gateway API"
+        );
+
+        context.Services.AddCors(options =>
+        {
+            options.AddDefaultPolicy(builder =>
+            {
+                builder
+                    .WithOrigins(
+                        configuration["App:CorsOrigins"]
+                            .Split(",", StringSplitOptions.RemoveEmptyEntries)
+                            .Select(o => o.Trim().RemovePostFix("/"))
+                            .ToArray()
+                    )
+                    .WithAbpExposedHeaders()
+                    .SetIsOriginAllowedToAllowWildcardSubdomains()
+                    .AllowAnyHeader()
+                    .AllowAnyMethod()
+                    .AllowCredentials();
+            });
+        });
+    }
+
+    public override void OnApplicationInitialization(ApplicationInitializationContext context)
+    {
+        var app = context.GetApplicationBuilder();
+        var env = context.GetEnvironment();
+
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+        app.UseCorrelationId();
+        app.UseAbpSerilogEnrichers();
+        app.UseCors();
+        app.UseSwaggerUIWithYarp(context);
+
+        app.UseRewriter(new RewriteOptions()
+            // Regex for "", "/" and "" (whitespace)
+            .AddRedirect("^(|\\|\\s+)$", "/swagger"));
+
+        app.UseRouting();
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapReverseProxy();
+        });
+    }
+}
